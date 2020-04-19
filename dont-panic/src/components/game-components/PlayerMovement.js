320ca68068a33813c5028002da40ec6342aaf427
HALEY_BUBLEY <HaleyBubley@gmail.com>
2020-04-18 21:16:05 -0500

    Added player movement info

--- dont-panic/src/components/game-components/PlayerMovement.js
+++ dont-panic/src/components/game-components/PlayerMovement.js
@@ -3,7 +3,9 @@ import React from 'react'
 export default function PlayerMovement() {
     return (
         <div className="player-movement-container">
-            
+            <div className="player-grid">
+                <i class="fad fa-ufo"></i>
+            </div>
         </div>
     )
 }
