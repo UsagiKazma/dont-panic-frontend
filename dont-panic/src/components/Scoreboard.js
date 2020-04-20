8b93e26b210a1501075a30cd9bc4663082cd379c
willowmoon <douglaskelley1001@gmail.com>
2020-04-20 10:25:11 -0400

    added scoreboard background

--- dont-panic/src/components/Scoreboard.js
+++ dont-panic/src/components/Scoreboard.js
@@ -2,9 +2,12 @@ import React from 'react'
 
 function ScoreBoard() {
     return (
-        <div>
+        <>
+        <div className="App-header">
             <h1>Scoreboard</h1>
         </div>
+        <div className="scoreboard-background"></div>
+        </>
     )
 }
 
