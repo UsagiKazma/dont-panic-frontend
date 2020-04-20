879e98cd85c98a83511f0cf06f93387bcf8b035b
willowmoon <douglaskelley1001@gmail.com>
2020-04-20 17:20:13 -0400

    css change

--- dont-panic/src/components/game-components/FailureDisplay.js
+++ dont-panic/src/components/game-components/FailureDisplay.js
@@ -4,7 +4,7 @@ export default function FailureDisplay({ failures }) {
   let failureDisplay = []
   if(failures>0){
    failureDisplay = [...Array(failures)].map((e, i) => {
-        return <p key={i} className="failures-p"><i id="failure-icon" className="fal fa-skull"></i></p>;
+        return <p key={i} className="failures-p"><i id="failure-icon" className="fad fa-alien-monster"></i></p>;
       })
       return failureDisplay;
     }
