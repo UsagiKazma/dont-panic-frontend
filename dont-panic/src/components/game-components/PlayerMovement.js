3f872c623459431ee92a616b13f99acad86c0e9f
HALEY_BUBLEY <HaleyBubley@gmail.com>
2020-04-20 14:12:25 -0500

    Added failure conditions

--- dont-panic/src/components/game-components/PlayerMovement.js
+++ dont-panic/src/components/game-components/PlayerMovement.js
@@ -3,9 +3,8 @@ import React from 'react'
 export default function PlayerMovement({percent}) {
     return (
         <div className="player-movement-container">
-            
             <div className="player-grid" style={{width: `${percent}%`}}>
-                <i class="fad fa-ufo"></i>
+                <i className="fad fa-ufo"></i>
             </div>
             <div className="background"></div>
         </div>
--- dont-panic/src/components/game-components/TypingCheck.js
+++ dont-panic/src/components/game-components/TypingCheck.js
@@ -1,16 +1,25 @@
 import React, { useState, useEffect } from "react";
+import { Redirect } from "react-router-dom";
 import KeyPress from "./KeyPress";
 import { currentTime } from "../../game-utility/time";
 import { generate } from "../../game-utility/words";
+import FailureDisplay from "./FailureDisplay";
 const initialWords = generate();
 
-export default function TypingCheck({ handleWPM, handleLevel, handleWordCount, wordCount, level }) {
+export default function TypingCheck({
+  handleWPM,
+  handleLevel,
+  handleWordCount,
+  wordCount,
+  level,
+}) {
   const [indexLetter, setIndexLetter] = useState(1);
   const [word, setWord] = useState(initialWords);
   const [letter, setLetter] = useState(word.charAt(0));
   const [startTime, setStartTime] = useState();
-  const [keyInput, setKeyInput] = useState("");
-  const handleTypedChar = input => {
+  const [keyInput, setKeyInput] = useState();
+  const [failures, setFailures] = useState(-1);
+  const handleTypedChar = (input) => {
     setKeyInput(input);
   };
   useEffect(() => {
@@ -21,6 +30,9 @@ export default function TypingCheck({ handleWPM, handleLevel, handleWordCount, w
       setLetter(word.charAt(indexLetter));
       let newIndex = indexLetter + 1;
       setIndexLetter(newIndex);
+    } else if (keyInput !== letter) {
+      setFailures(failures + 1);
+      console.log("failure", failures);
     }
     if (word.charAt(indexLetter) === "" && keyInput === letter) {
       handleWordCount(wordCount + 1);
@@ -41,9 +53,15 @@ export default function TypingCheck({ handleWPM, handleLevel, handleWordCount, w
 
   return (
     <>
-      <header className="App-header">
+      <header className="typing-check">
+        <div className="failure-display">
+          {failures === 6 && <Redirect push to="/userInput" />}
+          <FailureDisplay failures={failures} />
+        </div>
         <p className="letters">
-          <span className="current-letter glitch-game" data-text={letter}>{letter}</span>
+          <span className="current-letter glitch-game" data-text={letter}>
+            {letter}
+          </span>
           <span className="current-word">{word.substr(indexLetter)}</span>
         </p>
         <KeyPress handleTypedChar={handleTypedChar} keyInput={keyInput} />
