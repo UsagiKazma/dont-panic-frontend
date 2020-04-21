187a6f6257c99353abdb8818416bb87334e4137e
willowmoon <douglaskelley1001@gmail.com>
2020-04-21 14:55:19 -0400

    got api to work

--- dont-panic/src/components/Canvas.js
+++ dont-panic/src/components/Canvas.js
@@ -2,17 +2,18 @@ import React, {useState, useEffect} from 'react'
 import Game from "./game-components/Game"
 import "../App.scss"
 import {getAllWords} from "../game-utility/api-helper"
-function Canvas() {
 
+
+function Canvas() {
     const [wordsArr, setWordsArr] = useState('')
     useEffect(() => {
       const makeAPICall = async () => {
         const resp =  await getAllWords()
         setWordsArr(resp) 
-        console.log('useEffect - resp', wordsArr)
       }
       makeAPICall()
     }, [])
+    
     
     return (
         <div>
