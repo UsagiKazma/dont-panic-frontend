aa51f9e183935d478e0fb4c8986bfd3e26f809f3
Jguzman <juan@DESKTOP-DILVCG8.localdomain>
2020-04-21 17:23:25 -0400

    starting filter

--- dont-panic/src/components/game-components/Game.js
+++ dont-panic/src/components/game-components/Game.js
@@ -14,6 +14,7 @@ export default function Game({ wordsArr }) {
 
   if (wordsArr && wordsArr.length) {
     //generate needed wordsArr to conditionally render, pulling it in here works
+
     const generate = (wordsArr) => {
       var random = wordsArr[Math.floor(Math.random()*wordsArr.length)]
       console.log('words.js random ',random)
@@ -21,7 +22,51 @@ export default function Game({ wordsArr }) {
     };
     //call generate each time random is hit
     let random = generate(wordsArr)
-    console.log('random', random)
+    // console.log('random', random)
+
+   // wordDifficulty filtered mapped then filtered 
+    const easy = wordsArr.filter(level =>{if(level && level.difficulty === 'easy'){return level.word}})
+    const easyWords = easy.map(words => {return words.word})
+    // const medium = wordsArr.map(level =>{if(level && level.difficulty === 'medium'){return level.word}})
+    // const mediumWords = medium.map(words => {return words.word})
+    // const hard = wordsArr.map(level =>{if(level && level.difficulty === 'hard'){return level.word}})
+    // const hardWords = hard.map(words => {return words.word})
+    // const dangerous = wordsArr.map(level =>{if(level && level.difficulty === 'dangerous'){return level.word}})
+    // const dangerousWords = dangerous.map(words => {return words.word})
+    console.log('EEEASY MONEEY',easyWords)
+
+  //1-3 easy
+  //4-6 medium
+  //6-9 hard
+  //9-12 dangerous
+  // const random = generate(easy)
+
+    if(level <=3){
+      let random = generate(easy)
+      console.log('Game-if-statement-easy',random)
+      console.log('Level at the time', level)
+    }else if(level <=6 && level > 4){
+      let random = generate(medium)
+      // console.log('Game-if-statement-medium',random)
+      // console.log('Level at the time', level)
+    }else if(level <=9 && level > 6){
+      let random = generate(hard)
+      // console.log('Game-if-statement-hard',random)
+      // console.log('Level at the time', level)
+    }else if(level <=12 && level > 9){
+      let random = generate(dangerous)
+      // console.log('Game-if-statement-dangerous',random)
+      // console.log('Level at the time', level)
+    }
+
+
+   console.log('Game - randomAfterIf - ',random)
+
+
+//1-3 easy
+//4-6 medium
+//6-9 hard
+//9-12 dangerous
 
       const handleWPM = (input) => {
         setWpm(input);
