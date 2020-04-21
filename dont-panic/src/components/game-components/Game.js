d6f9de318ab9f3c6918913431715a4d81a066685
Jguzman <juan@DESKTOP-DILVCG8.localdomain>
2020-04-21 18:16:27 -0400

    Sktitybabpababp

--- dont-panic/src/components/game-components/Game.js
+++ dont-panic/src/components/game-components/Game.js
@@ -11,6 +11,7 @@ export default function Game({ wordsArr }) {
   const [wordCount, setWordCount] = useState(0);
   const [percent, setPercent] = useState(0);
   const [playerScore, setPlayerScore] = useState(0);
+  const [random, setrandom] = useState()
 
   if (wordsArr && wordsArr.length) {
     //generate needed wordsArr to conditionally render, pulling it in here works
@@ -20,41 +21,48 @@ export default function Game({ wordsArr }) {
       console.log('words.js random ',random)
       return (random && random.word)
     };
+
+    
     //call generate each time random is hit
-    let random = generate(wordsArr)
+    // let random = generate(wordsArr)
     // console.log('random', random)
 
+
    // wordDifficulty filtered mapped then filtered 
-    const easy = wordsArr.filter(level =>{if(level && level.difficulty === 'easy'){return level.word}})
-    const easyWords = easy.map(words => {return words.word})
-    // const medium = wordsArr.map(level =>{if(level && level.difficulty === 'medium'){return level.word}})
-    // const mediumWords = medium.map(words => {return words.word})
-    // const hard = wordsArr.map(level =>{if(level && level.difficulty === 'hard'){return level.word}})
-    // const hardWords = hard.map(words => {return words.word})
-    // const dangerous = wordsArr.map(level =>{if(level && level.difficulty === 'dangerous'){return level.word}})
-    // const dangerousWords = dangerous.map(words => {return words.word})
-    console.log('EEEASY MONEEY',easyWords)
+    let easy = wordsArr.filter(level =>{if(level && level.difficulty === 'easy'){return level.word}})
+    let easyWords = easy.map(words => {return words.word})
+
+    let medium = wordsArr.filter(level =>{if(level && level.difficulty === 'medium'){return level.word}})
+    let mediumWords = medium.map(words => {return words.word})
+
+    let hard = wordsArr.filter(level =>{if(level && level.difficulty === 'hard'){return level.word}})
+    let hardWords = hard.filter(words => {return words.word})
+
+    // let dangerous = wordsArr.map(level =>{if(level && level.difficulty === 'dangerous'){return level.word}})
+    // let dangerousWords = dangerous.map(words => {return words.word})
+
+    // console.log('EEEASY MONEEY',easyWords)
+    setrandom(generate(easy))
 
   //1-3 easy
   //4-6 medium
   //6-9 hard
   //9-12 dangerous
-  // const random = generate(easy)
 
-    if(level <=3){
-      let random = generate(easy)
-      console.log('Game-if-statement-easy',random)
-      console.log('Level at the time', level)
+    if(level){
+      setrandom(generate(easyWords))
+      // console.log('Game-if-statement-easy',random)
+      // console.log('Level at the time', level)
     }else if(level <=6 && level > 4){
-      let random = generate(medium)
+      setrandom(generate(mediumWords))
       // console.log('Game-if-statement-medium',random)
       // console.log('Level at the time', level)
     }else if(level <=9 && level > 6){
-      let random = generate(hard)
+      setrandom(generate(hardWords))
       // console.log('Game-if-statement-hard',random)
       // console.log('Level at the time', level)
     }else if(level <=12 && level > 9){
-      let random = generate(dangerous)
+      setrandom()
       // console.log('Game-if-statement-dangerous',random)
       // console.log('Level at the time', level)
     }
