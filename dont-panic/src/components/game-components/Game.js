470a31541b8eb88999b932f4888b839c11877505
Jguzman <juan@DESKTOP-DILVCG8.localdomain>
2020-04-21 20:04:11 -0400

    fixed?

--- dont-panic/src/components/game-components/Game.js
+++ dont-panic/src/components/game-components/Game.js
@@ -11,7 +11,7 @@ export default function Game({ wordsArr }) {
   const [wordCount, setWordCount] = useState(0);
   const [percent, setPercent] = useState(0);
   const [playerScore, setPlayerScore] = useState(0);
-  const [random, setrandom] = useState()
+
 
   if (wordsArr && wordsArr.length) {
     //generate needed wordsArr to conditionally render, pulling it in here works
@@ -24,7 +24,7 @@ export default function Game({ wordsArr }) {
 
     
     //call generate each time random is hit
-    // let random = generate(wordsArr)
+    let random = generate(wordsArr)
     // console.log('random', random)
 
 
@@ -42,33 +42,33 @@ export default function Game({ wordsArr }) {
     // let dangerousWords = dangerous.map(words => {return words.word})
 
     // console.log('EEEASY MONEEY',easyWords)
-    setrandom(generate(easy))
+
 
   //1-3 easy
   //4-6 medium
   //6-9 hard
   //9-12 dangerous
 
-    if(level){
-      setrandom(generate(easyWords))
-      // console.log('Game-if-statement-easy',random)
-      // console.log('Level at the time', level)
-    }else if(level <=6 && level > 4){
-      setrandom(generate(mediumWords))
-      // console.log('Game-if-statement-medium',random)
-      // console.log('Level at the time', level)
-    }else if(level <=9 && level > 6){
-      setrandom(generate(hardWords))
-      // console.log('Game-if-statement-hard',random)
-      // console.log('Level at the time', level)
-    }else if(level <=12 && level > 9){
-      setrandom()
-      // console.log('Game-if-statement-dangerous',random)
-      // console.log('Level at the time', level)
-    }
+  //   if(level){
+  //     setrandom(generate(easyWords))
+  //     // console.log('Game-if-statement-easy',random)
+  //     // console.log('Level at the time', level)
+  //   }else if(level <=6 && level > 4){
+  //     setrandom(generate(mediumWords))
+  //     // console.log('Game-if-statement-medium',random)
+  //     // console.log('Level at the time', level)
+  //   }else if(level <=9 && level > 6){
+  //     setrandom(generate(hardWords))
+  //     // console.log('Game-if-statement-hard',random)
+  //     // console.log('Level at the time', level)
+  //   }else if(level <=12 && level > 9){
+  //     setrandom()
+  //     // console.log('Game-if-statement-dangerous',random)
+  //     // console.log('Level at the time', level)
+  //   }
 
 
-   console.log('Game - randomAfterIf - ',random)
+  //  console.log('Game - randomAfterIf - ',random)
 
 
 //1-3 easy
